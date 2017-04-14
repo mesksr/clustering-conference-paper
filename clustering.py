dcc7af7d0bb468a21c7b79ecfb888583e7b33259
Shubham Kumar Singh Rajput <livingdeadbody@gmail.com>
2017-04-14 23:03:44 +0530

    Start

---
+++ clustering.py
@@ -0,0 +1,139 @@
+import os
+
+def get_distance(word_fr1, word_fr2):
+    #--- paper is already merged ---#
+    if (word_fr1 is None or word_fr2 is None):
+        return -float('inf')
+    
+    ans = 0
+    for i in range(no_of_words):
+        ans += word_fr1[i]*word_fr2[i]
+    return ans
+    
+fin = open("data", 'r')
+temp_data = fin.readlines()
+#print "\ntemp data :", temp_data, "has", len(temp_data), "lines"
+
+#--- Number of Papers ---#
+no_of_papers = len(temp_data)//3
+print "\nno of papers :", no_of_papers
+
+no_of_words = len(temp_data[-2].split())
+print "\nno of words :", no_of_words
+
+titles = []
+authors = []
+word_frs = []
+
+#--- Separating Title, Author, Word-Frequency ---#
+print "\noriginal word_frs :"
+for i in range(no_of_papers):
+    
+    title_curr, author_curr = temp_data[i*3].split('&')
+    title_curr = title_curr.split(':')[1].strip()
+    author_curr = author_curr.split(':')[1].strip()
+
+    word_fr_curr = map(int, temp_data[i*3 + 1].split())
+    word_fr_curr = word_fr_curr + [0]*(no_of_words - len(word_fr_curr))
+
+    print '>>>', word_fr_curr, ':', title_curr, 'by', author_curr
+    
+    titles.append(title_curr)
+    authors.append(author_curr)
+    word_frs.append(word_fr_curr)
+
+#--- Finding Max Fr ---#
+max_fr = 0
+for i in range(no_of_papers):
+    max_fr = max(max_fr, max(word_frs[i]))
+
+#--- Calculating Equalizer ---#
+eq = max_fr/2
+
+print "\nmax fr :", max_fr
+print "eq :", eq
+
+#--- Modifying words_frs ---#
+for i in range(no_of_papers):
+    for j in range(no_of_words):
+        word_frs[i][j] -= eq
+
+print "\nmodified word_frs :"
+for i in range(no_of_papers):
+    print '>>>', word_fr_curr, ':', titles[i], 'by', authors[i]
+    
+
+parent = {}
+for i in range(no_of_papers):
+    parent[i] = i
+
+def find(x):
+    while (parent[x] != x):
+        x = parent[x]
+    return x
+
+def union(i, j):
+    pi = find(i)
+    pj = find(j)
+    parent[pj] = pi
+
+#--- Forming cluster ---#
+no_of_papers_temp = no_of_papers
+while (True):
+    matrix = []
+    for i in range(no_of_papers_temp):
+        matrix.append(['_']*no_of_papers_temp)
+
+    #--- Finding closest pair ---#        
+    curr_closest_pair = None
+    curr_closest_dist = -float('inf')
+
+    for i in range(no_of_papers_temp):
+        for j in range(i+1, no_of_papers_temp):
+            matrix[i][j] = get_distance(word_frs[i], word_frs[j])
+            if (matrix[i][j] > curr_closest_dist):
+                curr_closest_dist = matrix[i][j]
+                curr_closest_pair = (i, j)
+                
+    print "\n*************************************"    
+    print "\nclosest pair :", curr_closest_pair    
+    print "\ndistances :"
+    for i in range(no_of_papers_temp):
+        for j in range(no_of_papers_temp):
+            print matrix[i][j], '\t', 
+        print ''
+
+    #--- Terminating condition ---#
+    if (curr_closest_dist <= 0):
+        print "\nno two paper are close enough to cluster"
+        break
+
+    #--- Merge current closest pair ---#
+    p1 = curr_closest_pair[0]
+    p2 = curr_closest_pair[1]
+    union(p1, p2)
+
+    no_of_papers_temp -= 1
+
+    #print word_frs, '->', 
+    for i in range(no_of_words):
+        word_frs[p1][i] = (word_frs[p1][i] + word_frs[p2][i])/2
+    word_frs[p2] = None
+    #print word_frs
+
+print "\n*************************************\n"
+
+count = 1
+for i in range(no_of_papers):
+    if (i == find(i)):
+        print "cluster", count
+        for j in range(i, no_of_papers):
+            if (find(j) == i):
+                print titles[j], 'by', authors[j]
+        count += 1
+        print ''
+    
+
+    
+    
+     
---
+++ pre-process.py
@@ -0,0 +1,151 @@
+import os
+from nltk.stem.snowball import SnowballStemmer
+
+def sort_utility(x):
+    return x[1]
+
+def get_keywords_from_abstract(string, n):
+    #--- Returns 'n' most-frequent keywords and their frequencies from the 'string' ---#
+    
+    abstract_word_list = string.split()
+    
+    temp_wf = {}
+    for word in abstract_word_list:
+        word = str(stemmer.stem(word))
+        if (word not in stop_words):
+            temp_wf[word] = temp_wf.get(word, 0) + 1
+
+    sorted_wf = sorted(temp_wf.items(), reverse = True, key = sort_utility)
+
+    word_list = []
+    freq_list = []
+    for i in range(n):
+        word_list.append(sorted_wf[i][0])
+        freq_list.append(sorted_wf[i][1])
+
+    return word_list, freq_list
+
+def get_keywords_listed(string):
+    #--- Returns all the keywords listed in the 'string' and setting frequency = 20---#
+    keywords = string.split(',')
+    for i in range(len(keywords)):
+        keywords[i] = str(stemmer.stem(keywords[i].strip()))
+    return keywords
+
+#--- Cleaning old data ---#
+fin = open("data", "w")
+fin.close()
+
+fin = open("words", "w")
+fin.close()
+
+#--- Creating Stemmer ---#
+stemmer = SnowballStemmer('english')
+
+#--- Getting the list of stop-words ---#
+fsw = open("stopwords", 'r')
+stop_words = fsw.readlines()
+for i in range(len(stop_words)):
+    #--- Cleaning stop-words ---#
+    stop_words[i] = str(stemmer.stem(stop_words[i].strip()))
+fsw.close()
+
+#--- Change directory to Papers ---#
+os.chdir(os.getcwd() + '/Papers')
+
+#--- Read all files and add to data ---#
+file_names = os.listdir(os.getcwd())
+
+no_of_words_to_extract_from_abstract = 5
+
+for file_name in file_names:
+    title = None
+    author = None 
+    abstract = None
+    keywords = None
+    if (file_name.endswith('.txt')):
+        fin = open(file_name, 'r')
+        for line in fin:
+            #--- Trim blank lines ---#
+            if (len(line) > 1):
+                if (line.startswith('Title')):
+                    title = line.split(':')[1].strip()
+                elif (line.startswith('Author')):
+                    author = line.split(':')[1].strip()
+                elif (line.startswith('Abstract')):
+                    abstract = line.split(':')[1].strip()
+                elif (line.startswith('Keywords')):
+                    keywords = line.split(':')[1].strip()
+        fin.close()
+        
+        #--- Get additional keywords ---#
+        keywords_from_abstract, frequencies_from_abstract = get_keywords_from_abstract(abstract, no_of_words_to_extract_from_abstract)
+        keywords_listed = get_keywords_listed(keywords)
+ 
+        #--- Changing directory going outside 'Papers' directory ---#
+        os.chdir(os.getcwd()[:-7]) 
+
+        #--- Read all words from 'words' file ---#
+        fin_words = open('words', 'r')
+        words = fin_words.readlines()
+        fin_words.close()
+
+        #--- Combining keywords (from abstract and those listed) and frequencies ---#   
+        keywords_combined = []
+        frequencies_combined = []
+        weight_of_keywords = 10
+        for word in keywords_from_abstract + keywords_listed:
+            #--- Skipping repeated words ---#
+            if (word in keywords_combined):
+                continue
+            if (word in keywords_from_abstract and word in keywords_listed):
+                keywords_combined.append(word)
+                frequencies_combined.append(frequencies_from_abstract[keywords_from_abstract.index(word)] + weight_of_keywords)
+            elif (word in keywords_from_abstract):
+                keywords_combined.append(word)
+                frequencies_combined.append(frequencies_from_abstract[keywords_from_abstract.index(word)])
+            else:
+                keywords_combined.append(word)
+                frequencies_combined.append(weight_of_keywords)
+
+        
+        #--- Printing temp data ---#
+        print title, author, ':', keywords_combined, '->', frequencies_combined
+
+        
+        #--- Calculating frequencies of existing words --- #
+        visited  = [False]*len(keywords_combined) #--- To track the new words >> all the words not visited at the end are new words ---#
+
+        frequency_for_curr_file = []
+        for word in words:
+            word = word.strip() #--- removing '\n' from the end ---#
+            if word in keywords_combined:
+                pos = keywords_combined.index(word)
+                visited[pos] = True
+                frequency_for_curr_file.append(frequencies_combined[pos])
+            else:
+                frequency_for_curr_file.append(0)
+
+        #--- Adding new words and their frequencies ---#
+        fout_words = open('words', 'a') #--- To add new words ---#
+        for i in range(len(keywords_listed)):
+            if (not visited[i]):
+                fout_words.write(keywords_listed[i].strip()+'\n')
+                frequency_for_curr_file.append(frequencies_combined[i])
+        fout_words.close()
+
+        #--- Adding data to the 'data' file ---#
+        fout_data = open('data', 'a')
+        fout_data.write('title : ' + title + ' &  author : ' + author + '\n')
+        temp = map(str, frequency_for_curr_file)
+        fout_data.write(' '.join(temp) + '\n')
+        fout_data.write('\n')
+        fout_data.close()    
+ 
+        #--- Changing directory going inside 'Papers' directory ---#
+        os.chdir(os.getcwd() + '/Papers')
+    else:
+        print 'ERROR :', file_name, 'is in wrong format'
+    
+    print '----------'*5
+    
